9522607077561a7743634231c560502c0950cd8f
BrianaLentz <98199391+BrianaLentz@users.noreply.github.com>
2022-03-16 14:37:02 -0700

    axios pokemon api

--- src/components/FetchPokemon.jsx
+++ src/components/FetchPokemon.jsx
@@ -1,21 +1,29 @@
 import React, {useState} from 'react'
+import axios from 'axios'
 
 
 const FetchPokemon = () => {
     const [pokemons, setPokemons] = useState()
 
-
-    const fetchPokemons = () => {
-        fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=807`)
-        .then(response => {
-            return response.json()
-        })
-        .then(responseJson => {
-            console.log(responseJson)
-            setPokemons(responseJson.results)
+    const fetchPokemons = () =>{
+        axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=807`)
+        .then(response =>{
+            console.log(response.data.results)
+            setPokemons(response.data.results)
         })
         .catch(err => console.log(err))
     }
+    // const fetchPokemons = () => {
+    //     fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=807`)
+    //     .then(response => {
+    //         return response.json()
+    //     })
+    //     .then(responseJson => {
+    //         console.log(responseJson)
+    //         setPokemons(responseJson.results)
+    //     })
+    //     .catch(err => console.log(err))
+    // }
     return(
         
         <>
@@ -23,7 +31,7 @@ const FetchPokemon = () => {
 
         {pokemons ?
         
-            pokemons.map((pokemon, i)=>{
+            pokemons.map((pokemon)=>{
                 return(
                     <ul key={pokemon.url}>
                         <li>{pokemon.name}</li>
